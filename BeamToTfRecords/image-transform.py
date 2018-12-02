fa240ae233b7b38a55ff238d4c67f84de857b6f4
Damien Pontifex <damien.pontifex@gmail.com>
2018-12-02 15:57:02 +1000

    updates to beam example

--- BeamToTfRecords/image-transform.py
+++ BeamToTfRecords/image-transform.py
@@ -4,10 +4,9 @@ from __future__ import print_function
 import os
 import apache_beam as beam
 import tensorflow as tf
-import numpy as np
-from PIL import Image
-import io
 from argparse import ArgumentParser
+
+tf.enable_eager_execution()
 
 class ImageSource(beam.io.filebasedsource.FileBasedSource):
     """A source fo reading image files."""
@@ -18,11 +17,12 @@ class ImageSource(beam.io.filebasedsource.FileBasedSource):
     def read_records(self, file_name, offset_range_tracker):
         with self.open_file(file_name) as file_to_read:
             image_bytes = file_to_read.read()
-            img = Image.open(io.BytesIO(image_bytes)).convert('RGB')
+            img = tf.image.decode_jpeg(image_bytes, channels=3).numpy()
             
+            # Get the string label from its parent folder
             label = os.path.basename(os.path.dirname(file_name)) if self._has_label else None
 
-            yield img, label
+            yield img, label, file_name
 
 
 class ReadFromImage(beam.PTransform):
@@ -42,8 +42,7 @@ class TFExampleFromImageDoFn(beam.DoFn):
         self._has_label = has_label
 
     def process(self, element):
-        img, label = element
-        assert isinstance(img, Image.Image)
+        img, label, file_name = element
 
         def _bytes_feature(value):
             return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
@@ -54,13 +53,14 @@ class TFExampleFromImageDoFn(beam.DoFn):
         def _float_feature(value):
             return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
         
-        pixels = np.array(img).tobytes()
-        width, height = img.size
+        pixels = img.tobytes()
+        width, height, _ = img.shape
 
         feature = {
-            'image': _bytes_feature(pixels),
-            'width': _int_feature(width),
-            'height': _int_feature(height)
+            'image/bytes': _bytes_feature(pixels),
+            'image/width': _int_feature(width),
+            'image/height': _int_feature(height),
+            'image/filename': _bytes_feature(file_name)
         }
 
         if self._has_label:
@@ -70,12 +70,20 @@ class TFExampleFromImageDoFn(beam.DoFn):
 
         yield example
 
-def main(args):
+def run_pipeline():
+    parser = ArgumentParser(description='Create TFRecord files from directories using Apache Beam')
+
+    parser.add_argument('--input_directory', help='Base directory for images')
+    parser.add_argument('--output_directory', help='Base directory for tfrecord output')
+    parser.add_argument('--dataset_name', help='Name of the dataset. Used for file naming conventions')
+    parser.add_argument('--num_shards', default=2, type=int, help='Number of shards to split the dataset into')
+    parser.add_argument('--has_labels', action='store_true', help='Whether the folder structure includes labels')
+    args, pipeline_args = parser.parse_known_args()
+
     path_expression = os.path.join(args.input_directory, '**/*.jpg')
     output_path = os.path.join(args.output_directory, args.dataset_name)
 
-    pipeline_options = beam.pipeline.PipelineOptions()
-    with beam.Pipeline(options=pipeline_options) as p: 
+    with beam.Pipeline(argv=pipeline_args) as p: 
         images = p | ReadFromImage(path_expression, args.has_labels)
         
         _ = (
@@ -88,13 +96,4 @@ def main(args):
         )
 
 if __name__ == '__main__':
-    parser = ArgumentParser(description='Create TFRecord files from directories using Apache Beam')
-
-    parser.add_argument('--input_directory', help='Base directory for images')
-    parser.add_argument('--output_directory', help='Base directory for tfrecord output')
-    parser.add_argument('--dataset_name', help='Name of the dataset. Used for file naming conventions')
-    parser.add_argument('--num_shards', default=2, type=int, help='Number of shards to split the dataset into')
-    parser.add_argument('--has_labels', action='store_true', help='Whether the folder structure includes labels')
-    args = parser.parse_args()
-
-    main(args)
+    run_pipeline()
